b9766c714640e2c4e5f2bd7dbd00d4292d210cd1
tushushu <m18591915960@163.com>
2018-07-06 15:03:28 +0800

    finish gbdt

--- gbdt.py
+++ gbdt.py
@@ -5,20 +5,45 @@
 @Last Modified by: tushushu 
 @Last Modified time: 2018-07-05 17:37:34 
 """
-
-
-class Node(object):
-    def __init__(self):
-        raise NotImplementedError
+from regression_tree import RegressionTree
+from copy import copy
+from utils import load_boston_house_prices, train_test_split, get_r2, run_time
 
 
 class GBDT(object):
     def __init__(self):
-        raise NotImplementedError
+        self.trees = None
+
+    def fit(self, X, y, n_estimators, max_depth, min_samples_split):
+        self.trees = []
+        residual = copy(y)
+        for _ in range(n_estimators):
+            tree = RegressionTree()
+            tree.fit(X, residual, max_depth, min_samples_split)
+            residual = [a - b for a, b in zip(residual, tree.predict(X))]
+            self.trees.append(tree)
+
+    def _predict(self, Xi):
+        return sum(tree._predict(Xi) for tree in self.trees)
+
+    def predict(self, X):
+        return [self._predict(Xi) for Xi in X]
 
 
+@run_time
 def main():
-    raise NotImplementedError
+    print("Tesing the accuracy of GBDT...")
+    # Load data
+    X, y = load_boston_house_prices()
+    # Split data randomly, train set rate 70%
+    X_train, X_test, split_train, split_test = train_test_split(
+        X, y, random_state=10)
+    # Train model
+    reg = GBDT()
+    reg.fit(X=X_train, y=split_train, n_estimators=10,
+            max_depth=2, min_samples_split=2)
+    # Model accuracy
+    get_r2(reg, X_test, split_test)
 
 
 if __name__ == "__main__":
--- regression_tree.py
+++ regression_tree.py
@@ -6,7 +6,7 @@
 @Last Modified time: 2018-07-05 17:51:04 
 """
 from copy import copy
-from utils import load_boston_house_prices, train_test_split, get_r2, run_time, min_max_scale
+from utils import load_boston_house_prices, train_test_split, get_r2, run_time
 
 
 class Node(object):
@@ -36,8 +36,8 @@ class RegressionTree(object):
         self.root = Node()
         self.height = 0
 
-    def _get_split_var(self, X, y, idx, feature, split):
-        """Calculate the average of each set when x is splitted into two pieces.
+    def _get_split_mse(self, X, y, idx, feature, split):
+        """Calculate the mse of each set when x is splitted into two pieces.
         D(X) = E{[X-E(X)]^2} = E(X^2)-[E(X)]^2 
 
         Arguments:
@@ -51,29 +51,29 @@ class RegressionTree(object):
             list -- Average of splitted x
         """
 
-        y_sum = [0, 0]
-        y_cnt = [0, 0]
-        y_sqr_sum = [0, 0]
+        split_sum = [0, 0]
+        split_cnt = [0, 0]
+        split_sqr_sum = [0, 0]
         # Iterate each row and compare with the split point
         for i in idx:
             xi, yi = X[i][feature], y[i]
             if xi < split:
-                y_cnt[0] += 1
-                y_sum[0] += yi
-                y_sqr_sum[0] += yi ** 2
+                split_cnt[0] += 1
+                split_sum[0] += yi
+                split_sqr_sum[0] += yi ** 2
             else:
-                y_cnt[1] += 1
-                y_sum[1] += yi
-                y_sqr_sum[1] += yi ** 2
-        # Calculate the variance of y
-        y_avg = [y_sum[0] / y_cnt[0], y_sum[1] / y_cnt[1]]
-        y_var = [y_sqr_sum[0] - y_sum[0] * y_avg[0],
-                 y_sqr_sum[1] - y_sum[1] * y_avg[1]]
-        return sum(y_var), split, y_avg
+                split_cnt[1] += 1
+                split_sum[1] += yi
+                split_sqr_sum[1] += yi ** 2
+        # Calculate the mse of y
+        split_avg = [split_sum[0] / split_cnt[0], split_sum[1] / split_cnt[1]]
+        split_mse = [split_sqr_sum[0] - split_sum[0] * split_avg[0],
+                     split_sqr_sum[1] - split_sum[1] * split_avg[1]]
+        return sum(split_mse), split, split_avg
 
     def _choose_split_point(self, X, y, idx, feature):
         """Iterate each xi and split x, y into two pieces,
-        and the best split point is the xi when we get min variance.
+        and the best split point is the xi when we get min mse.
 
         Arguments:
             x {list} -- 1d list object with int or float
@@ -82,7 +82,7 @@ class RegressionTree(object):
             feature {int} -- Feature number
 
         Returns:
-            tuple -- The best choice of variance, feature, split point and average
+            tuple -- The best choice of mse, feature, split point and average
         """
         # Feature cannot be splitted if there's only one unique element.
         unique = set([X[i][feature] for i in idx])
@@ -90,13 +90,13 @@ class RegressionTree(object):
             return None
         # In case of empty split
         unique.remove(min(unique))
-        # Get split point which has min variance
-        y_var, split, y_avg = min((self._get_split_var(X, y, idx, feature, split)
-                                   for split in unique), key=lambda x: x[0])
-        return y_var, feature, split, y_avg
+        # Get split point which has min mse
+        mse, split, split_avg = min((self._get_split_mse(X, y, idx, feature, split)
+                                     for split in unique), key=lambda x: x[0])
+        return mse, feature, split, split_avg
 
     def _choose_feature(self, X, y, idx):
-        """Choose the feature which has min variance.
+        """Choose the feature which has min mse.
 
         Arguments:
             X {list} -- 2d list object with int or float
@@ -108,13 +108,13 @@ class RegressionTree(object):
         """
 
         m = len(X[0])
-        # Compare the variance of each feature and choose best one.
+        # Compare the mse of each feature and choose best one.
         split_rets = [x for x in map(lambda x: self._choose_split_point(
             X, y, idx, x), range(m)) if x is not None]
         # Terminate if no feature can be splitted
         if split_rets == []:
             return None
-        _, feature, split, y_avg = min(
+        _, feature, split, split_avg = min(
             split_rets, key=lambda x: x[0])
         # Get split idx into two pieces and empty the idx.
         idx_split = [[], []]
@@ -125,7 +125,7 @@ class RegressionTree(object):
                 idx_split[0].append(i)
             else:
                 idx_split[1].append(i)
-        return feature, split, y_avg, idx_split
+        return feature, split, split_avg, idx_split
 
     def _expr2literal(self, expr):
         """Auxiliary function of print_rules.
@@ -201,9 +201,9 @@ class RegressionTree(object):
             if feature_rets is None:
                 continue
             # Split
-            nd.feature, nd.split, y_avg, idx_split = feature_rets
-            nd.left = Node(y_avg[0])
-            nd.right = Node(y_avg[1])
+            nd.feature, nd.split, split_avg, idx_split = feature_rets
+            nd.left = Node(split_avg[0])
+            nd.right = Node(split_avg[1])
             que.append([depth+1, nd.left, idx_split[0]])
             que.append([depth+1, nd.right, idx_split[1]])
         # Update tree depth and rules
@@ -217,7 +217,7 @@ class RegressionTree(object):
         for i, rule in enumerate(self.rules):
             literals, score = rule
             print("Rule %d: " % i, ' | '.join(
-                literals) + ' => y_hat %.4f' % score)
+                literals) + ' => split_hat %.4f' % score)
 
     def _predict(self, row):
         """Auxiliary function of predict.
@@ -255,16 +255,16 @@ def main():
     print("Tesing the accuracy of LinearRegression(batch)...")
     # Load data
     X, y = load_boston_house_prices()
-    X = min_max_scale(X)
     # Split data randomly, train set rate 70%
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=10)
+    X_train, X_test, split_train, split_test = train_test_split(
+        X, y, random_state=10)
     # Train model
     reg = RegressionTree()
-    reg.fit(X=X_train, y=y_train, max_depth=4)
+    reg.fit(X=X_train, y=split_train, max_depth=4)
     # Show rules
     reg.print_rules()
     # Model accuracy
-    get_r2(reg, X_test, y_test)
+    get_r2(reg, X_test, split_test)
 
 
 if __name__ == "__main__":
