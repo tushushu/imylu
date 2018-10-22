1830cd1e7b7b659d85f2eeb1d522997fff6ca4e9
tushushu <m18591915960@163.com>
2018-10-22 17:03:17 +0800

    make result printing more pretty.

--- examples/als_example.py
+++ examples/als_example.py
@@ -11,22 +11,32 @@ os.chdir(os.path.split(os.path.realpath(__file__))[0])
 import sys
 sys.path.append(os.path.abspath(".."))
 
-from imylu.utils import load_movie_ratings
+from imylu.utils import load_movie_ratings, run_time
 from imylu.recommend.als import ALS
 
 
+def format_prediction(item_id, score):
+    return "item_id:%d score:%.2f" % (item_id, score)
+
+
+@run_time
 def main():
     print("Tesing the accuracy of ALS...")
     # Load data
     X = load_movie_ratings()
     # Train model
     model = ALS()
-    model.fit(X, k=3, max_iter=4)
+    model.fit(X, k=3, max_iter=5)
+    print()
+
+    print("Showing the predictions of users...")
     # Predictions
     user_ids = range(1, 5)
     predictions = model.predict(user_ids, n_items=2)
     for user_id, prediction in zip(user_ids, predictions):
-        print("User id:%d\nrecommedation: %s" % (user_id, prediction))
+        _prediction = [format_prediction(item_id, score)
+                       for item_id, score in prediction]
+        print("User id:%d recommedation: %s" % (user_id, _prediction))
 
 
 if __name__ == "__main__":
