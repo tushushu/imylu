332f08897593981bc249e8ceba9ee9f474be5b04
tushushu <liutienan@soyoung.com>
2019-05-22 15:41:45 +0800

    finish gbdt classifier

--- examples/gbdt_classifier_example.py
+++ examples/gbdt_classifier_example.py
@@ -3,7 +3,7 @@
 @Author: tushushu
 @Date: 2018-08-21 14:33:11
 @Last Modified by:   tushushu
-@Last Modified time: 2018-08-21 14:33:11
+@Last Modified time: 2019-05-22 15:41:11
 """
 import os
 os.chdir(os.path.split(os.path.realpath(__file__))[0])
@@ -19,17 +19,19 @@ from imylu.utils.utils import run_time
 
 @run_time
 def main():
+    """Tesing the performance of GBDT classifier"""
+
     print("Tesing the performance of GBDT classifier...")
     # Load data
-    X, y = load_breast_cancer()
+    data, label = load_breast_cancer()
     # Split data randomly, train set rate 70%
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=20)
+    data_train, data_test, label_train, label_test = train_test_split(data, label, random_state=20)
     # Train model
     clf = GradientBoostingClassifier()
-    clf.fit(X_train, y_train, n_estimators=2,
+    clf.fit(data_train, label_train, n_estimators=2,
             learning_rate=0.8, max_depth=3, min_samples_split=2)
     # Model evaluation
-    model_evaluation(clf, X_test, y_test)
+    model_evaluation(clf, data_test, label_test)
 
 
 if __name__ == "__main__":
