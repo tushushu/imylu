dc80aba3136a5cd4188fd706b77f51f51c6c400c
tushushu <liutienan@soyoung.com>
2019-05-30 14:56:59 +0800

    nn draft

---
+++ imylu/neural_network/sigmoid_node.py
@@ -0,0 +1,22 @@
+"""
+@Author: tushushu
+@Date: 2019-05-30 14:44:33
+"""
+import numpy as np
+from .base_node import Node
+
+
+class Sigmoid(Node):
+    def _sigmoid(self, x):
+        return 1. / (1. + np.exp(-x))
+
+    def forward(self):
+        input_value = self.inbound_nodes[0].value
+        self.value = self._sigmoid(input_value)
+
+    def backward(self):
+        self.gradients = {n: np.zeros_like(n.value) for n in self.inbound_nodes}
+        for n in self.outbound_nodes:
+            grad_cost = n.gradients[self]
+            sigmoid = self.value
+            self.gradients[self.inbound_nodes[0]] += sigmoid * (1 - sigmoid) * grad_cost
